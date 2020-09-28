144b604c686c86c66d3e28da6268b4c660cd765a
Federico Paletta <federico.paletta88@gmail.com>
2020-09-28 12:09:02 +0200

    search and location screens

---
+++ src/screens/LocationScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import CenteredContainer from '../../components/container/CenteredContainer';
+
+const LocationScreen: React.FC = () => {
+  return (
+    <CenteredContainer>
+      <Text>Location Screen</Text>
+    </CenteredContainer>
+  );
+};
+
+export default LocationScreen;
---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import CenteredContainer from '../../components/container/CenteredContainer';
+
+const SearchScreen: React.FC = () => {
+  return (
+    <CenteredContainer>
+      <Text>Search Screen</Text>
+    </CenteredContainer>
+  );
+};
+
+export default SearchScreen;
