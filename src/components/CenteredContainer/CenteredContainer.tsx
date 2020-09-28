dc585b25699f85ae202fbfc58219c59444100433
Federico Paletta <federico.paletta88@gmail.com>
2020-09-28 19:14:45 +0200

    containers

---
+++ src/components/CenteredContainer/CenteredContainer.tsx
@@ -0,0 +1,13 @@
+import {View} from 'react-native';
+import styled from 'styled-components';
+
+const CenteredContainer = styled(View)`
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f0f0f0;
+`;
+
+export default CenteredContainer;
---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,13 @@
+import {View} from 'react-native';
+import styled from 'styled-components';
+
+const Container = styled(View)`
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 50px;
+  background-color: #f0f0f0;
+`;
+
+export default Container;
